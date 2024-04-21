fd65198f065e7ff5b1a2a706614d63ae67ea39fa
yoonnable <yoonnable@gmail.com>
2024-04-21 19:08:36 +0900

    init: create project

---
+++ src/main/java/com/ecommerce/ECommerceMallApplication.java
@@ -0,0 +1,11 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceMallApplication.class, args);
+    }
+}
