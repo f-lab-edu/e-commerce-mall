14bccb93404d6b5a64544f7ebf7477f0bf98ca0d
yoonnable <yoonnable@gmail.com>
2024-04-23 01:04:42 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/com/ecommerce/member/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.member.controller;
+
+import com.ecommerce.member.dto.SignupRequest;
+import com.ecommerce.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("")
+    public ResponseEntity signup(@Valid @RequestBody SignupRequest request) {
+        memberService.save(request);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/ecommerce/member/service/MemberService.java
@@ -0,0 +1,34 @@
+package com.ecommerce.member.service;
+
+import com.ecommerce.member.dto.SignupRequest;
+import com.ecommerce.member.entity.Member;
+import com.ecommerce.member.repository.MemberRepository;
+import com.ecommerce.utils.SHA256Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long save(SignupRequest request) {
+        Optional<Member> checkEmail = memberRepository.findByEmail(request.getEmail());
+        if(checkEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+        return memberRepository.save(Member.builder()
+                .email(request.getEmail())
+                .password(SHA256Util.encrypt(request.getPassword()))
+                .name(request.getName())
+                .phone(request.getPhone())
+                .regDt(LocalDateTime.now())
+                .modDt(LocalDateTime.now()).build()).getId();
+    }
+}
