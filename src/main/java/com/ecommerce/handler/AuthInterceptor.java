f4ad8b5310bdefcbbf0259e431072b0d2ad408c9
yoonnable <yoonnable@gmail.com>
2024-04-28 23:55:41 +0900

    feat: 로그인 체크 인터셉터 구현

---
+++ src/main/java/com/ecommerce/handler/AuthInterceptor.java
@@ -0,0 +1,23 @@
+package com.ecommerce.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+
+        Object loginMembersId = session.getAttribute("LOGIN_MEMBERS_ID");
+
+        if(loginMembersId == null) {
+            throw new IllegalArgumentException("로그인이 필요합니다.");
+
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/ecommerce/handler/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.ecommerce.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/members/*", "/orders/*").excludePathPatterns("/members");
+    }
+}
