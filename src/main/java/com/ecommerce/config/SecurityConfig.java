908fa56cb56521da737633bfd485242a0038b672
yoonnable <yoonnable@gmail.com>
2024-05-13 23:50:12 +0900

    feat: Spring Security + JWT FWT Config - csrf disable - formLogin disable - httpBasic disable - session stateless

--- src/main/java/com/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,12 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        // 검색, 조회는 비회원, 회원 모두 허용
-                        .requestMatchers("/", "/categories/**", "/products/**", "/search").permitAll()
-                        // 그 외 모든 기능은 회원만 허용
-                        .anyRequest().authenticated());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests((auth) -> auth
+                // 검색, 조회, 로그인, 회원가입은 비회원, 회원 모두 허용
+                .requestMatchers("/", "/categories/**", "/products/**", "/search", "/members", "/signin").permitAll()
+                // 그 외 모든 기능은 회원만 허용
+                .anyRequest().authenticated()
+        );
+
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+
+
         return http.build();
     }
 }
