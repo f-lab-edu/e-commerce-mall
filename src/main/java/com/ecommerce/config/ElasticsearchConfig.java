c40df269743844a238d9929ed988e0510b542f3b
yoonnable <yoonnable@gmail.com>
2024-06-11 22:04:40 +0900

    feat: Elasticsearch 연동

---
+++ src/main/java/com/ecommerce/config/ElasticsearchConfig.java
@@ -0,0 +1,20 @@
+package com.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.data.elasticsearch.url}")
+    private String elasticsearchUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl)
+                .build();
+    }
+}
