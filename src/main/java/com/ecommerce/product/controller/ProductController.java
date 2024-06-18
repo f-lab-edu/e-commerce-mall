c0b256dcc7e135087dfc88874de01ee387f0c12a
yoonnable <yoonnable@gmail.com>
2024-06-18 16:52:43 +0900

    refactor: Product Response 변환 코드 service에서 controller로 이동

--- src/main/java/com/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.ecommerce.product.controller;
 
 import com.ecommerce.product.dto.ProductsResponse;
 import com.ecommerce.product.dto.SortType;
+import com.ecommerce.product.entity.Product;
 import com.ecommerce.product.service.ProductService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +12,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+  private final ProductService productService;
 
-    @GetMapping("/{categoryId}/{sortKey}")
-    public ResponseEntity<List<ProductsResponse>> readProducts(@PathVariable Long categoryId, @PathVariable SortType sortKey) {
-        List<ProductsResponse> products = productService.readProducts(categoryId, sortKey);
-        return ResponseEntity.ok().body(products);
-    }
+  @GetMapping("/{categoryId}/{sortKey}")
+  public ResponseEntity<List<ProductsResponse>> readProducts(@PathVariable Long categoryId,
+      @PathVariable SortType sortKey) {
+    List<Product> products = productService.readProducts(categoryId, sortKey);
+    return ResponseEntity.ok().body(products.stream().map(ProductsResponse::new).toList());
+  }
 }
