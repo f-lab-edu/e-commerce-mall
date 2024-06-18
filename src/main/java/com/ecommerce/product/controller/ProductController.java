538301355e53c879418c7a722a4bd25de40ebbc5
yoonnable <yoonnable@gmail.com>
2024-06-18 17:14:21 +0900

    refactor: Search Response 변환 코드 service에서 controller로 이동

--- src/main/java/com/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -1,26 +1,40 @@
 package com.ecommerce.product.controller;
 
+import com.ecommerce.product.document.ProductDocument;
 import com.ecommerce.product.dto.DeliveryType;
+import com.ecommerce.product.dto.ProductsSearchResponse;
 import com.ecommerce.product.dto.SortType;
 import com.ecommerce.product.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+  private final ProductService productService;
 
-    @GetMapping("/{sortKey}")
-    public ResponseEntity<Map<String, Object>> search(@PathVariable SortType sortKey,
-                                                      @RequestParam String keyword,
-                                                      @RequestParam(required = false) DeliveryType deliveryType) {
-        Map<String, Object> result = productService.search(keyword, deliveryType, sortKey);
-        return ResponseEntity.ok().body(result);
-    }
+  @GetMapping("/{sortKey}")
+  public ResponseEntity<Map<String, Object>> search(@PathVariable SortType sortKey,
+      @RequestParam String keyword,
+      @RequestParam(required = false) DeliveryType deliveryType) {
+    SearchHits<ProductDocument> searchHits = productService.search(keyword, deliveryType, sortKey);
+
+    Map<String, Object> result = new HashMap<>();
+    result.put("count", searchHits.getTotalHits());
+    List<ProductsSearchResponse> productsSearchResponses = searchHits.getSearchHits().stream()
+        .map(hit -> new ProductsSearchResponse(hit.getContent())).toList();
+    result.put("data", productsSearchResponses);
+    return ResponseEntity.ok().body(result);
+  }
 }
--- src/main/java/com/ecommerce/product/service/ProductService.java
+++ src/main/java/com/ecommerce/product/service/ProductService.java
@@ -3,11 +3,12 @@ package com.ecommerce.product.service;
 import co.elastic.clients.elasticsearch._types.FieldValue;
 import com.ecommerce.product.document.ProductDocument;
 import com.ecommerce.product.dto.DeliveryType;
-import com.ecommerce.product.dto.ProductsSearchResponse;
 import com.ecommerce.product.dto.SortType;
 import com.ecommerce.product.entity.Product;
 import com.ecommerce.product.repository.ProductDocumentRepository;
 import com.ecommerce.product.repository.ProductRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -16,94 +17,85 @@ import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepository productRepository;
-    private final ProductDocumentRepository productDocumentRepository;
-    private final ElasticsearchOperations elasticsearchOperations;
 
-    /**
-     * 검색 service
-     *
-     * @param keyword
-     * @return map
-     */
-    public Map<String, Object> search(String keyword, DeliveryType deliveryType, SortType sortKey) {
+  private final ProductRepository productRepository;
+  private final ProductDocumentRepository productDocumentRepository;
+  private final ElasticsearchOperations elasticsearchOperations;
 
-        // 쿼리문 만들기
-        Query query = makeSearchQuery(keyword, deliveryType, sortKey);
+  /**
+   * 검색 service
+   *
+   * @param keyword
+   * @return map
+   */
+  public SearchHits<ProductDocument> search(String keyword, DeliveryType deliveryType,
+      SortType sortKey) {
 
-        // 검색 실행
-        SearchHits<ProductDocument> searchHits = elasticsearchOperations.search(query, ProductDocument.class);
+    // 쿼리문 만들기
+    Query query = makeSearchQuery(keyword, deliveryType, sortKey);
 
+    // 검색 실행
 
-        Map<String, Object> result = new HashMap<>();
+    return elasticsearchOperations.search(query, ProductDocument.class);
+  }
 
-        result.put("count", searchHits.getTotalHits());
+  /**
+   * 검색 쿼리문 생성
+   *
+   * @return query
+   */
+  private Query makeSearchQuery(String keyword, DeliveryType deliveryType, SortType sortKey) {
+    // 쿼리 생성
 
-        List<ProductsSearchResponse> productsSearchResponses = searchHits.getSearchHits().stream().map(hit -> new ProductsSearchResponse(hit.getContent())).toList();
+    // 키워드 적용할 필드 리스트
+    List<String> fields = new ArrayList<>();
+    fields.add("categoryName");
+    fields.add("name");
+    fields.add("brand");
 
-        result.put("data", productsSearchResponses);
-
-        return result;
+    List<NativeQuery> queries = new ArrayList<>();
+    // multi_match 쿼리문 생성
+    NativeQuery multiMathQuery = makeMultiMathQuery(keyword, fields);
+    queries.add(multiMathQuery);
+    if (deliveryType != null) {
+      // match 쿼리문 생성
+      NativeQuery termQuery = makeTermQuery(deliveryType.getFieldName(),
+          deliveryType.getFieldValue());
+      queries.add(termQuery);
     }
 
-    /**
-     * 검색 쿼리문 생성
-     *
-     * @return query
-     */
-    private Query makeSearchQuery(String keyword, DeliveryType deliveryType, SortType sortKey) {
-        // 쿼리 생성
+    return NativeQuery.builder()
+        .withQuery(q -> q.bool(b -> b.must(queries.stream().map(NativeQuery::getQuery).toList())))
+        .withSort(s -> s
+            .field(
+                p -> p
+                    .field(sortKey.getFieldName())
+                    .order(sortKey.getSortOrder())
+            )
+        ).build();
+  }
 
-        // 키워드 적용할 필드 리스트
-        List<String> fields = new ArrayList<>();
-        fields.add("categoryName");
-        fields.add("name");
-        fields.add("brand");
+  private NativeQuery makeMultiMathQuery(String keyword, List<String> fields) {
+    return NativeQuery.builder().withQuery(q -> q.multiMatch(m -> m.query(keyword).fields(fields)))
+        .build();
+  }
 
-        List<NativeQuery> queries = new ArrayList<>();
-        // multi_match 쿼리문 생성
-        NativeQuery multiMathQuery = makeMultiMathQuery(keyword, fields);
-        queries.add(multiMathQuery);
-        if (deliveryType != null) {
-            // match 쿼리문 생성
-            NativeQuery termQuery = makeTermQuery(deliveryType.getFieldName(), deliveryType.getFieldValue());
-            queries.add(termQuery);
-        }
+  private NativeQuery makeTermQuery(String fieldName, FieldValue value) {
+    return NativeQuery.builder().withQuery(q -> q.term(t -> t.field(fieldName).value(value)))
+        .build();
+  }
 
-        return NativeQuery.builder().withQuery(q -> q.bool(b -> b.must(queries.stream().map(NativeQuery::getQuery).toList())))
-                .withSort(s -> s
-                        .field(
-                                p -> p
-                                .field(sortKey.getFieldName())
-                                .order(sortKey.getSortOrder())
-                        )
-                ).build();
+
+  @Transactional
+  public void sync() {
+    List<Product> products = productRepository.findAll();
+
+    for (Product product : products) {
+      ProductDocument productDocument = new ProductDocument(product);
+      productDocumentRepository.save(productDocument);
     }
-
-    private NativeQuery makeMultiMathQuery(String keyword, List<String> fields) {
-        return NativeQuery.builder().withQuery(q -> q.multiMatch(m -> m.query(keyword).fields(fields))).build();
-    }
-
-    private NativeQuery makeTermQuery(String fieldName, FieldValue value) {
-        return NativeQuery.builder().withQuery(q -> q.term(t -> t.field(fieldName).value(value))).build();
-    }
-
-
-    @Transactional
-    public void sync() {
-        List<Product> products = productRepository.findAll();
-
-        for (Product product : products) {
-            ProductDocument productDocument = new ProductDocument(product);
-            productDocumentRepository.save(productDocument);
-        }
-    }
+  }
 }
