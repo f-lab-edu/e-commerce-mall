364e5a5913856da8e1b68e3febea59a23dbbbcfd
yoonnable <yoonnable@gmail.com>
2024-06-27 12:42:28 +0900

    feat: Jacoco 코드 커버리지 툴 도입
    - Jacoco 플러그인 추가
    - 회원가입 테스트 코드 Speing security 적용 코드에 맞춰 암호화 변경

--- src/test/java/com/ecommerce/member/service/MemberServiceTest.java
+++ src/test/java/com/ecommerce/member/service/MemberServiceTest.java
@@ -1,9 +1,11 @@
 package com.ecommerce.member.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.ecommerce.member.dto.SignupRequest;
 import com.ecommerce.member.entity.Member;
 import com.ecommerce.member.repository.MemberRepository;
-import com.ecommerce.utils.SHA256Util;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,71 +13,72 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class MemberServiceTest {
 
-    @Autowired
-    MemberService memberService;
+  @Autowired
+  MemberService memberService;
 
-    @Autowired
-    MemberRepository memberRepository;
+  @Autowired
+  MemberRepository memberRepository;
+  @Autowired
+  BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @BeforeEach
-    public void setUp() {
-        memberRepository.deleteAll();
-    }
+  @BeforeEach
+  public void setUp() {
+    memberRepository.deleteAll();
+  }
 
-    @DisplayName("회원가입에 성공한다.")
-    @Test
-    void save() {
-        // given
-        final SignupRequest request = SignupRequest.builder()
-                .email("member1@test.com")
-                .password("f_lab16881577")
-                .name("테스트1")
-                .phone("01011111111")
-                .build();
+  @DisplayName("회원가입에 성공한다.")
+  @Test
+  void save() {
+    // given
+    final SignupRequest request = SignupRequest.builder()
+        .email("member1@test.com")
+        .password("f_lab16881577")
+        .name("테스트1")
+        .phone("01011111111")
+        .build();
 
-        // when
-        memberService.save(request);
+    // when
+    memberService.save(request);
 
-        // then
-        List<Member> members = memberRepository.findAll();
+    // then
+    List<Member> members = memberRepository.findAll();
 
-        assertThat(members.size()).isEqualTo(1);
-        assertThat(members.get(0).getEmail()).isEqualTo(request.getEmail());
-        assertThat(members.get(0).getPassword()).isEqualTo(SHA256Util.encrypt(request.getPassword()));
-        assertThat(members.get(0).getName()).isEqualTo(request.getName());
-        assertThat(members.get(0).getPhone()).isEqualTo(request.getPhone());
-    }
+    assertThat(members.size()).isEqualTo(1);
+    assertThat(members.get(0).getEmail()).isEqualTo(request.getEmail());
+    assertThat(bCryptPasswordEncoder.matches(request.getPassword(),
+        members.get(0).getPassword())).isTrue();
+    assertThat(members.get(0).getName()).isEqualTo(request.getName());
+    assertThat(members.get(0).getPhone()).isEqualTo(request.getPhone());
+  }
 
-    @DisplayName("이메일이 중복되면 회원가입에 실패한다.")
-    @Test
-    void saveWithDuplicateEmail() {
-        // given
-        final String url = "/members";
-        final SignupRequest request1 = SignupRequest.builder()
-                .email("member1@test.com")
-                .password("f_lab16881577")
-                .name("테스트1")
-                .phone("01011111111")
-                .build();
+  @DisplayName("이메일이 중복되면 회원가입에 실패한다.")
+  @Test
+  void saveWithDuplicateEmail() {
+    // given
+    final String url = "/members";
+    final SignupRequest request1 = SignupRequest.builder()
+        .email("member1@test.com")
+        .password("f_lab16881577")
+        .name("테스트1")
+        .phone("01011111111")
+        .build();
 
-        final SignupRequest request2 = SignupRequest.builder()
-                .email("member1@test.com")
-                .password("f_lab16881577")
-                .name("테스트2")
-                .phone("01011111111")
-                .build();
+    final SignupRequest request2 = SignupRequest.builder()
+        .email("member1@test.com")
+        .password("f_lab16881577")
+        .name("테스트2")
+        .phone("01011111111")
+        .build();
 
-        memberService.save(request1);
+    memberService.save(request1);
 
-        // when - then
-        Assertions.assertThrows(DataIntegrityViolationException.class, () -> memberService.save(request2));
-    }
+    // when - then
+    Assertions.assertThrows(DataIntegrityViolationException.class,
+        () -> memberService.save(request2));
+  }
 }
