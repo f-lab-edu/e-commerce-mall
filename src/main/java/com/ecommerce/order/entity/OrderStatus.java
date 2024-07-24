e7a3ed6bec38301f19d5bb2b9e6f4285993b7a16
yoonnable <yoonnable@gmail.com>
2024-07-24 17:27:49 +0900

    refactor: OrderStatus 리팩토링

--- src/main/java/com/ecommerce/order/entity/OrderStatus.java
+++ src/main/java/com/ecommerce/order/entity/OrderStatus.java
@@ -1,27 +1,20 @@
 package com.ecommerce.order.entity;
 
 public enum OrderStatus {
-  PENDING,    // 주문 대기 중
-  PROCESSING, // 주문 처리 중
-  SHIPPED,    // 주문 배송 중
-  DELIVERED,  // 주문 배송 완료
-  CANCELED;   // 주문 취소
+  PENDING("Order is pending"),    // 주문 대기 중
+  PROCESSING("Order is being processed"), // 주문 처리 중
+  SHIPPED("Order has been shipped"),    // 주문 배송 중
+  DELIVERED("Order has been delivered"),  // 주문 배송 완료
+  CANCELED("Order has been canceled");   // 주문 취소
+
+  private final String message;
+
+  OrderStatus(String message) {
+    this.message = message;
+  }
 
   @Override
   public String toString() {
-    switch (this) {
-      case PENDING:
-        return "Order is pending";
-      case PROCESSING:
-        return "Order is being processed";
-      case SHIPPED:
-        return "Order has been shipped";
-      case DELIVERED:
-        return "Order has been delivered";
-      case CANCELED:
-        return "Order has been canceled";
-      default:
-        return "Unknown status";
-    }
+    return this.message;
   }
 }
